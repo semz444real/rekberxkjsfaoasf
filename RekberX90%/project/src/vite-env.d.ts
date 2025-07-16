62d206a16ffd5360621420a370f0bf543bdca5a6
semz444real <rukiryake@gmail.com>
2025-07-17 05:58:10 +0700

    Update vite-env.d.ts

--- RekberX90%/project/src/vite-env.d.ts
+++ RekberX90%/project/src/vite-env.d.ts
@@ -1,1 +1,2 @@
-/// <reference types="vite/client" />
+VITE_SUPABASE_URL=https://nggdvoxpjrpbbbtcvrol.supabase.co
+VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nZ2R2b3hwanJwYmJidGN2cm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDM4MTYsImV4cCI6MjA2ODI3OTgxNn0.oJ03pEnAjBl1pnJgz6Iphn5Kd1y-iGMIEOEwf8epooY
